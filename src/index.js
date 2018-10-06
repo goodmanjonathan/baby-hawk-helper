6f0e2e3029ad7886a0029b0e31e0a245e8a14d10
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-10-05 20:02:54 -0500

    fix import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DrawerTest from './components/drawertest';
+import DrawerTest from './Components/DrawerTest';
 
 ReactDOM.render(
   <DrawerTest />,
