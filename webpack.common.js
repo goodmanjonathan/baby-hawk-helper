4410334ccd404f136873591b590f91ee8a1b5c48
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-10-23 20:14:29 -0500

    set up webpack production script

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new 
+    HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+          });
+		  
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist', // maybe path.resolve(__dirname, 'dist') ?
+        filename: 'index_bundle.js'
+    },
+    plugins: [
+        HTMLWebpackPluginConfig
+    ],
+    module: { 
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+        }
+        ]
+    }
+}
