9e5634eca522c6e6fa6a800d65257d127f6dccff
Matthew Browder <mbrowder97@gmail.com>
2018-11-02 14:26:43 -0500

    Updated CSS and Drawer Layout

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { rules } = require('webpack-atoms');
 
-var HTMLWebpackPluginConfig = new 
-    HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            inject: 'body'
-          });
-		  
+var HTMLWebpackPluginConfig = new
+		HtmlWebpackPlugin({
+						template: './src/index.html',
+						filename: 'index.html',
+						inject: 'body'
+					});
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist', // maybe path.resolve(__dirname, 'dist') ?
-        filename: 'index_bundle.js'
-    },
-    plugins: [
-        HTMLWebpackPluginConfig
-    ],
-    module: { 
-        rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-        }
-        ]
-    }
+		entry: './src/index.js',
+		output: {
+				path: __dirname + '/dist', // maybe path.resolve(__dirname, 'dist') ?
+				filename: 'index_bundle.js'
+		},
+		plugins: [
+				HTMLWebpackPluginConfig
+		],
+		module: {
+				rules: [
+				rules.js(),
+				rules.images(),
+				rules.css(),
+				]
+		}
 }
