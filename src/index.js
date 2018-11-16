3e1b719891d598d48a0a15a788737b15bb9b2330
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-11-16 16:20:23 -0600

    current state of the login page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Layouts/App'
+import App from './Layouts/App';
+import Login from './Layouts/Login';
 
 ReactDOM.render(
-	<App />,
+	<Login />,
 	document.getElementById('root')
 );
