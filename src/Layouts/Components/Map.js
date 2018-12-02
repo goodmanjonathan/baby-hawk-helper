cfb24f9650f31dc7e60cbaeb24b5ea8bb258fd2f
Matthew Browder <mbrowder97@gmail.com>
2018-12-02 17:11:52 -0600

    fixing conflicts

--- src/Layouts/Components/Map.js
+++ src/Layouts/Components/Map.js
@@ -1,8 +1,8 @@
-
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Map, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
-import {features} from './features.js';
+import {firstFloor} from './firstFloor.js';
+import {secondFloor} from './secondFloor.js';
 
 const mapCenter = [29.57766548841692,-95.10419867344351];
 const zoomLevel = 19;
@@ -11,10 +11,13 @@ const schedule = {
 	rooms : [
 		'119B',
 		'140',
+		"216",
+
 	],
 	classes: {
 		'119B' : 'Class 1',
 		'140' : 'Class 2',
+		'216' : 'Class 3',
 	}
 }
 
@@ -23,6 +26,9 @@ export default class App extends Component {
 			// does this feature have a property named popupContent?
 			if (schedule.rooms.includes(feature.properties.Room)) {
 					layer.bindPopup(schedule.classes[feature.properties.Room]);
+			}
+			else{
+				layer.bindPopup(feature.properties.Type);
 			}
 		};
 		render() {
@@ -39,11 +45,25 @@ export default class App extends Component {
 											 attribution="&copy;<a>href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a>contributor"
 											/>
 										</LayersControl.BaseLayer>
-										<LayersControl.Overlay checked = 'true' name="Floor1">
-											<GeoJSON data = {features} style = {function(feature) {
+										<LayersControl.Overlay checked name="Floor1">
+											<GeoJSON data = {firstFloor} style = {function(feature) {
 													switch (feature.properties.Type) {
-													case 'Classroom': return {color: "#ff0000"};
-													case 'Office':   return {color: "#0000ff"};
+													case 'Classroom': return (schedule.rooms.includes(feature.properties.Room) ? {color: "#ff0000"} : {color: "#0078ad"});
+													case 'Office':   return {color: "#018744"};
+													case 'Lab': return {color: "#000000"};
+													case 'Restroom': return {color: "#bb33ff"}
+											}
+											}}
+											onEachFeature = {this.onEachFeature}
+											/>
+										</LayersControl.Overlay>
+										<LayersControl.Overlay name="Floor2">
+											<GeoJSON data = {secondFloor} style = {function(feature) {
+													switch (feature.properties.Type) {
+													case 'Classroom': return (schedule.rooms.includes(feature.properties.Room) ? {color: "#ff0000"} : {color: "#0078ad"});
+													case 'Office':   return {color: "#018744"};
+													case 'Lab': return {color: "#000000"};
+													case 'Restroom': return {color: "#bb33ff"}
 											}
 											}}
 											onEachFeature = {this.onEachFeature}
