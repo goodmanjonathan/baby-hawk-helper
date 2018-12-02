b88c6f9d710d096710e3932cc1c9da3a43c20305
Matthew Browder <mbrowder97@gmail.com>
2018-12-02 17:10:01 -0600

    fixing conflicts

--- src/client/Layouts/Components/Map.js
+++ src/client/Layouts/Components/Map.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/client/Layouts/Components/Map.js
 import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from "@material-ui/core/Paper";
@@ -143,3 +144,61 @@ export default class Map extends React.Component {
 		)
 	}
 }
+=======
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Map, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
+import {features} from './features.js';
+
+const mapCenter = [29.57766548841692,-95.10419867344351];
+const zoomLevel = 19;
+
+const schedule = {
+	rooms : [
+		'119B',
+		'140',
+	],
+	classes: {
+		'119B' : 'Class 1',
+		'140' : 'Class 2',
+	}
+}
+
+export default class App extends Component {
+			onEachFeature = (feature, layer) => {
+			// does this feature have a property named popupContent?
+			if (schedule.rooms.includes(feature.properties.Room)) {
+					layer.bindPopup(schedule.classes[feature.properties.Room]);
+			}
+		};
+		render() {
+				return (
+						<div>
+								<Map
+										center={mapCenter}
+										zoom={zoomLevel}
+								>
+									<LayersControl>
+										<LayersControl.BaseLayer name = "Map" checked = 'true'>
+											<TileLayer
+											url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+											 attribution="&copy;<a>href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a>contributor"
+											/>
+										</LayersControl.BaseLayer>
+										<LayersControl.Overlay checked = 'true' name="Floor1">
+											<GeoJSON data = {features} style = {function(feature) {
+													switch (feature.properties.Type) {
+													case 'Classroom': return {color: "#ff0000"};
+													case 'Office':   return {color: "#0000ff"};
+											}
+											}}
+											onEachFeature = {this.onEachFeature}
+											/>
+										</LayersControl.Overlay>
+									</LayersControl>
+								</Map>
+						</div>
+				);
+		}
+}
+>>>>>>> c13b911b... Working on implementing GeoJSON in Leaflet Map:src/Layouts/Components/Map.js
