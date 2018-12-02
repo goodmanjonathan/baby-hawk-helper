f1aac4ab6a4fe640867b91214481637a82d37ef8
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-12-01 21:15:55 -0600

    work on schedule cards

--- src/client/Layouts/Components/Schedule.js
+++ src/client/Layouts/Components/Schedule.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/Card";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import { ExpansionPanelDetails, ExpansionPanelSummary } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Paper from "@material-ui/core/Paper";
+import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
 	card: {
         minWidth: 275,
 	},
-    title: {
+    heading: {
         fontSize: 16,
         paddingLeft: 20,
         paddingTop: 7,
@@ -19,6 +23,10 @@ const styles = theme => ({
     body: {
         paddingLeft: 20,
         paddingBottom: 7,
+        color: "textSecondary",
+    },
+    panel: {
+
     }
 });
 
@@ -26,15 +34,27 @@ function makeClassCard(classes) {
     return (
         <Card className={classes.card} elevation={1}>
             <CardContent>
-                <Typography className={classes.title} gutterBottom>
-                    Design of Database Systems
-                </Typography>
-                <Typography className={classes.body} color="textSecondary">
-                    CSCI 4333.01<br/>
-                    Dr. Yue<br/>
-                    M 1:00-3:50<br/>
-                    Delta 241
-                </Typography>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>
+                            Design of Database Systems
+                        </Typography>
+                        <br/>
+                        <Typography className={classes.body}>
+                            CSCI 4333.01<br/>
+                            Dr. Yue<br/>
+                            M 1:00-3:50<br/>
+                            Delta 241
+                        </Typography>
+                    </ExpansionPanelSummary>
+
+                    <ExpansionPanelDetails>
+                        <Typography>
+                            111-111-1111<br/>
+                            yue@uhcl.edu
+                        </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             </CardContent>
         </Card>
     );
@@ -46,6 +66,7 @@ function Schedule(props) {
 	return (
 		<div>
             {makeClassCard(classes)}
+            <br/>
             {makeClassCard(classes)}
         </div>
 	);
