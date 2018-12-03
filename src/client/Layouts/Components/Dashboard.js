707838a2094b2ce64866482f56ced77fb2bc2fc2
Matthew Browder <mbrowder97@gmail.com>
2018-12-02 18:27:25 -0600

    Fixed Dashboard

--- src/client/Layouts/Components/Dashboard.js
+++ src/client/Layouts/Components/Dashboard.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import List from '@material-ui/core/List';
+import ListItem from "@material-ui/core/ListItem";
 import ListSubheader from '@material-ui/core/ListSubheader';
 import { Timeline } from 'react-twitter-widgets'
 import InstagramEmbed from 'react-instagram-embed'
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
 	root: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		width: '100%',
-		height: "100%",
-		//justifyContent: 'space-around',
-		overflow: 'hidden',
+		flexGrow: 1,
 		backgroundColor: theme.palette.background.paper,
+		maxWidth: 800
 	},
 	gridList: {
-		width: '100%',
-		height: '80%',
+		height: '100'
 	},
 	subheader: {
 		width: '100%',
@@ -31,39 +27,47 @@ function Dashboard(props) {
 	const { classes } = props;
 
 	return (
+		<Grid container justify = 'center'>
+			<Paper className = {classes.root}>
+				<List>
+					<ListItem >
+							<ListSubheader style={{textAlign: 'right'}} component="div"><p></p>
+							<a href="http://www.instagram.com/uhclearlake"><img src={require('./images/iconi.png')} width="40" height="40" /></a>
+							<span>  </span><a href="https://twitter.com/UHClearLake"><img src={require('./images/icont.png')} width="40" height="40" /></a>
+							<span>  </span><a href="https://www.facebook.com/UHClearLake/"><img src={require('./images/iconf.png')} width="40" height="40" /></a>
+							<span>  </span><a href="https://www.youtube.com/user/uhclearlake" ><img src={require('./images/icony.png')} width="40" height="40" /></a>
 
-	<Paper className = {classes.root}>
-
-			<GridList cellHeight={160} className={classes.gridList} cols={2}>
-			<GridListTile key="Subheader" cols={2} style={{ height: '60' }}>
-					<ListSubheader style={{textAlign: 'right'}} component="div"><p></p>
-					<a href="http://www.instagram.com/uhclearlake"><img src={require('./images/iconi.png')} width="40" height="40" /></a>
-					<span>  </span><a href="https://twitter.com/UHClearLake"><img src={require('./images/icont.png')} width="40" height="40" /></a>
-					<span>  </span><a href="https://www.facebook.com/UHClearLake/"><img src={require('./images/iconf.png')} width="40" height="40" /></a>
-					<span>  </span><a href="https://www.youtube.com/user/uhclearlake" ><img src={require('./images/icony.png')} width="40" height="40" /></a>
-
-					</ListSubheader>
-				</GridListTile>
-				<GridListTile key="Subheader" cols={2} style={{ height: '90%' }}>
-				<Timeline
-							dataSource={{
-								sourceType: 'profile',
-								screenName: 'UHClearLake'
-							}}
-							options={{
-								username: 'UHClearLake',
-								height: '100%',
-								width: '100%'
-							}}
-							onLoad={() => console.log('Timeline is loaded!')}
-						/>
-				</GridListTile>
-				<GridListTile key="Subheader" cols={2} style={{ height: '90%' }}>
-				<InstagramEmbed url='https://www.instagram.com/p/BqftGKEgS7I/' />
-				</GridListTile>
-
-			</GridList>
-		</Paper>
+							</ListSubheader>
+						</ListItem>
+						<Paper>
+							<ListItem>
+								<Grid container justify = 'center'>
+									<Timeline
+												dataSource={{
+													sourceType: 'profile',
+													screenName: 'UHClearLake'
+												}}
+												options={{
+													username: 'UHClearLake',
+													height: '500',
+													width: '95vh'
+												}}
+												onLoad={() => console.log('Timeline is loaded!')}
+											/>
+								</Grid>
+							</ListItem>
+						</Paper>
+						<Paper>
+							<ListItem>
+								<Grid container justify = 'center'>
+									<InstagramEmbed
+										url='https://www.instagram.com/p/BqftGKEgS7I/' />
+								</Grid>
+							</ListItem>
+						</Paper>
+					</List>
+				</Paper>
+			</Grid>
 	);
 }
 
--- src/client/Layouts/Components/Map.js
+++ src/client/Layouts/Components/Map.js
@@ -1,8 +1,20 @@
-
 import React, { Component } from 'react';
+import { withStyles } from "@material-ui/core/styles";
 import { render } from 'react-dom';
-import { Map, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
-import {features} from './features.js';
+import { Map, TileLayer, GeoJSON, LayersControl, Tooltip } from 'react-leaflet';
+import {firstFloor} from './firstFloor.js';
+import {secondFloor} from './secondFloor.js';
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import FiberManualRecordIcon from '@material-ui/icons/fibermanualrecord';
 
 const mapCenter = [29.57766548841692,-95.10419867344351];
 const zoomLevel = 19;
@@ -11,39 +23,109 @@ const schedule = {
 	rooms : [
 		'119B',
 		'140',
+		"216",
+
 	],
 	classes: {
 		'119B' : 'Class 1',
 		'140' : 'Class 2',
+		'216' : 'Class 3',
 	}
 }
 
-export default class App extends Component {
+const styles = theme => ({
+	avatar: {
+		margin: 10,
+	},
+	enrolledAvatar: {
+		margin: 10,
+		color: '#fff',
+		backgroundColor: "#ff0000",
+	},
+	classAvatar: {
+		margin: 10,
+		color: '#fff',
+		backgroundColor: "#0078ad",
+	},
+	officeAvatar: {
+		margin: 10,
+		color: '#fff',
+		backgroundColor: "#018744",
+	},
+	labAvatar: {
+		margin: 10,
+		color: '#fff',
+		backgroundColor: "#000000",
+	},
+	restroomAvatar: {
+		margin: 10,
+		color: '#fff',
+		backgroundColor: "#bb33ff",
+	},
+});
+
+
+class App extends Component {
+			state = {
+				legend: false,
+			}
 			onEachFeature = (feature, layer) => {
-			// does this feature have a property named popupContent?
 			if (schedule.rooms.includes(feature.properties.Room)) {
 					layer.bindPopup(schedule.classes[feature.properties.Room]);
 			}
+			else if (feature.properties.Faculty) {
+				layer.bindPopup(
+					"<b>" + feature.properties.Type + "</b><br/>Room: " + feature.properties.Room + "<br/>" + feature.properties.Faculty + "<br/>" + feature.properties.Email
+				);
+			}
+			else{
+				layer.bindPopup(
+					"<b>" + feature.properties.Type + "</b><br/>Room: " + feature.properties.Room
+				);
+			}
 		};
+		handleLegend = () => {
+			this.setState({legend: true});
+		};
+		handleClose = () => {
+			this.setState({legend: false});
+		}
 		render() {
+			const { classes } = this.props;
 				return (
 						<div>
 								<Map
 										center={mapCenter}
 										zoom={zoomLevel}
 								>
+
 									<LayersControl>
 										<LayersControl.BaseLayer name = "Map" checked = 'true'>
 											<TileLayer
 											url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 											 attribution="&copy;<a>href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a>contributor"
 											/>
+
 										</LayersControl.BaseLayer>
-										<LayersControl.Overlay checked = 'true' name="Floor1">
-											<GeoJSON data = {features} style = {function(feature) {
+										<LayersControl.Overlay checked name="Floor1">
+											<GeoJSON data = {firstFloor} style = {function(feature) {
 													switch (feature.properties.Type) {
-													case 'Classroom': return {color: "#ff0000"};
-													case 'Office':   return {color: "#0000ff"};
+													case 'Classroom': return (schedule.rooms.includes(feature.properties.Room) ? {color: "#ff0000"} : {color: "#0078ad"});
+													case 'Office':   return {color: "#018744"};
+													case 'Lab': return {color: "#000000"};
+													case 'Restroom': return {color: "#bb33ff"}
+											}
+											}}
+											onEachFeature = {this.onEachFeature}
+											/>
+										</LayersControl.Overlay>
+										<LayersControl.Overlay name="Floor2">
+											<GeoJSON data = {secondFloor} style = {function(feature) {
+													switch (feature.properties.Type) {
+													case 'Classroom': return (schedule.rooms.includes(feature.properties.Room) ? {color: "#ff0000"} : {color: "#0078ad"});
+													case 'Office':   return {color: "#018744"};
+													case 'Lab': return {color: "#000000"};
+													case 'Restroom': return {color: "#bb33ff"}
 											}
 											}}
 											onEachFeature = {this.onEachFeature}
@@ -51,7 +133,57 @@ export default class App extends Component {
 										</LayersControl.Overlay>
 									</LayersControl>
 								</Map>
+								<Button onClick = {this.handleLegend}>Legend</Button>
+								<Dialog open = {this.state.legend}
+									onClose = {this.handleClose}>
+									<DialogTitle>Legend</DialogTitle>
+									<DialogContent>
+										<List>
+											<ListItem>
+												<ListItemAvatar>
+													<Avatar className = {classes.enrolledAvatar}>
+														<FiberManualRecordIcon/>
+													</Avatar>
+												</ListItemAvatar>
+												<ListItemText>Enrolled Classes</ListItemText>
+											</ListItem>
+											<ListItem>
+												<ListItemAvatar>
+													<Avatar className = {classes.classAvatar}>
+														<FiberManualRecordIcon/>
+													</Avatar>
+												</ListItemAvatar>
+												<ListItemText>Classes</ListItemText>
+											</ListItem>
+											<ListItem>
+												<ListItemAvatar>
+													<Avatar className = {classes.officeAvatar}>
+														<FiberManualRecordIcon/>
+													</Avatar>
+												</ListItemAvatar>
+												<ListItemText>Offices</ListItemText>
+											</ListItem>
+											<ListItem>
+												<ListItemAvatar>
+													<Avatar className = {classes.labAvatar}>
+														<FiberManualRecordIcon/>
+													</Avatar>
+												</ListItemAvatar>
+												<ListItemText>Labs</ListItemText>
+											</ListItem>
+											<ListItem>
+												<ListItemAvatar>
+													<Avatar className = {classes.restroomAvatar}>
+														<FiberManualRecordIcon/>
+													</Avatar>
+												</ListItemAvatar>
+												<ListItemText>Restrooms</ListItemText>
+											</ListItem>
+										</List>
+									</DialogContent>
+								</Dialog>
 						</div>
 				);
 		}
 }
+export default withStyles(styles)(App);
