e100427a573760194add31f4259bef11a1b79907
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-12-05 12:24:03 -0600

    remove old login screen

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Layouts/App';
-import Login from './Layouts/Login';
-import Signup from './Layouts/Signup';
+//import Signup from './Layouts/Signup';
 
 ReactDOM.render(<App/>,
 	document.getElementById('root')
